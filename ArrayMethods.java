25fe793bb16d1e984c2f65ac136a78af700e40e1
sheinamara <ashein00@stuy.edu>
2018-11-03 13:58:21 -0400

    having trouble with figuring out different row lengths; some columns just don't have integers

---
+++ ArrayMethods.java
@@ -0,0 +1,48 @@
+public class ArrayMethods{
+   /*
+   *PART 1
+   */
+   public static int rowSum(int[][] ary, int x){
+     int finale = 0;
+     for (int i = 0; i < ary[x].length; i++){
+       finale += ary[x][i];
+     }
+     return finale;
+   }
+
+    //returns the sum of the elements in Row x of ary.
+
+   public static int columnSum(int[][] ary, int x){
+     int finale = 0;
+     for (int i = 0; i < ary.length; i++){
+
+       finale += ary[i][x];
+     }
+     return finale;
+   }
+    //returns the sum of the elements in Column x of ary (careful with rows of different lengths!).
+
+   /*
+   *PART 2 - use prior methods where appropriate
+   */
+//   public static int[] allRowSums(int[][] ary){}
+    //returns an array of the row sums for each row of ary.
+    //Index i of the return array contains the sum of elements in row i.
+
+//    public static int[] allColSums(int[][] ary){}
+    //Returns an array with the column sum of each column of ary.
+    //When a row is not long enough to reach the column count it as a zero. (NO indexOutOfBounds should ever occur)
+    //Index i of the return array contains the sum of elements in column i, ignoring any rows that are too short.
+    //The length of the returned array should be the length of the LONGEST array in ary.
+
+
+   /*
+   *PART 3 - use prior methods where appropriate
+   */
+//   public static boolean isRowMagic(int[][] ary){}
+     //checks if the array is row-magic (this means that every row has the same row sum).
+
+//   public static boolean isColumnMagic(int[][] ary){}
+    //checks if the array is column-magic (this means that every column has the same column sum).
+
+}
---
+++ Driver.java
@@ -0,0 +1,86 @@
+public class Driver{
+  public static String printArray(int[] arr) {
+    String output = "[";
+    for (int idx = 0; idx < arr.length; idx ++) {
+      output += arr[idx];
+      if (idx < arr.length - 1) {
+        output += ", ";
+      }
+    }
+    output += "]";
+    return output;
+  }
+  public static String printArray(int[][] arr) {
+    String output ="[[";
+    for (int x = 0; x < arr.length; x ++) {
+      for (int y = 0; y < arr[x].length; y ++) {
+        output += arr[x][y];
+        if (y < arr[x].length - 1) {
+          output += ", ";
+        }
+      }
+      if (x < arr.length - 1) {
+        output += "], [";
+      }
+    }
+    output += "]]";
+    return output;
+  }
+  public static void main(String[] args) {
+    int[][] a = {
+      {1,2,3,4},
+      {3,5,7,2,4},
+      {6,17,3}
+    };
+    int[][] b = {
+      {1,2,3,4},
+      {3,4,3}
+    };
+    int[][] c = {
+      {1,5,8,19},
+      {16,12,9,-2}
+    };
+    System.out.println("Array a: " + printArray(a));
+    System.out.println("Array b: " + printArray(b));
+    System.out.println("Array c: " + printArray(c) + "\n");
+    System.out.println("Testing Part 1\n");
+    System.out.println("Finding the sum of each row");
+    System.out.println("First Row: should return 10");
+    System.out.println(ArrayMethods.rowSum(a,0));
+    System.out.println("Second Row: should return 21");
+    System.out.println(ArrayMethods.rowSum(a,1));
+    System.out.println("Third Row: should return 26");
+    System.out.println(ArrayMethods.rowSum(a,2) + "\n");
+    System.out.println("Finding the sum of each column");
+    System.out.println("First Column: should return 10");
+    System.out.println(ArrayMethods.columnSum(a,0));
+    System.out.println("Second Column: should return 24");
+    System.out.println(ArrayMethods.columnSum(a,1));
+    System.out.println("Third Column: should return 13");
+    System.out.println(ArrayMethods.columnSum(a,2));
+    System.out.println("Fourth Column: should return 6");
+    System.out.println(ArrayMethods.columnSum(a,3));
+    System.out.println("Fifth Column: should return 4");
+    System.out.println(ArrayMethods.columnSum(a,4) + "\n\n");
+/*
+    System.out.println("Testing Part 2\n");
+    System.out.println("Putting all the row and column sums into arrays");
+    System.out.println("Array of row sums: Should return [10, 21, 26]");
+    System.out.println(printArray(ArrayMethods.allRowSums(a)));
+    System.out.println("Array of column sums: Should return [10, 24, 13, 6, 4]");
+    System.out.println(printArray(ArrayMethods.allColSums(a)) + "\n\n");
+    System.out.println("Testing Part 3\n");
+    System.out.println("Testing whether each array has \"row magic\"");
+    System.out.println("Array a: should return false");
+    System.out.println(ArrayMethods.isRowMagic(a));
+    System.out.println("Array b: should return true");
+    System.out.println(ArrayMethods.isRowMagic(b) + "\n");
+    System.out.println("Testing whether each array has \"column magic\"");
+    System.out.println("Array a: should return false");
+    System.out.println(ArrayMethods.isColumnMagic(a));
+    System.out.println("Array c: should return true");
+    System.out.println(ArrayMethods.isColumnMagic(c) + "\n");
+    System.out.println("Woohoo good job!");
+*/
+  }
+}
